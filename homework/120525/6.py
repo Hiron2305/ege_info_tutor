b7b7076f04dd93378aa31a677409f688b63302f2
Hiron2305 <mrHiron@yandex.ru>
2025-05-17 18:33:10 +0300

    commit

--- homework/120525/6.py
+++ homework/120525/6.py
@@ -11,7 +11,7 @@ total_best = total - discount_best
 
 prices.sort()
 group_count = len(prices) // 6
-discount_worst = sum(prices[i] for i in range(group_count))
+discount_worst = sum(prices[i] // 2 for i in range(group_count))
 total_worst = total - discount_worst
 
 print(total_best, total_worst)
